d101bb474c88006d9ed4ef4bd1c47218866814c6
jianzzha <jianzzha@redhat.com>
2020-04-26 10:39:44 -0400

    update redfish_update_pxe.py

--- redfish_update_pxe.py
+++ redfish_update_pxe.py
@@ -6,13 +6,28 @@ import re
 pxe_device = sys.argv[1]
 with open("boot_devices.txt", "r") as f:
     boot_array = json.load(f)
-    dev_index = len(boot_array) - 1 
     for item in boot_array:
-        if item["Name"] != pxe_device and re.match("NIC", item["Name"]) and item["Index"] < dev_index:
-            item["Enabled"] = False 
-        elif item["Name"] == pxe_device:
-            dev_index = item["Index"]
+        if item["Name"] == pxe_device:
+            expected_pxedev_current_index = item["Index"]
+        if re.match("Disk", item["Name"]):
+            disk_current_index = item["Index"]
+    if disk_current_index == 0 and expected_pxedev_current_index == 1:
+        # exit 0 means no update
+        sys.exit(0)   
+    for item in boot_array:
+        if re.match("Disk", item["Name"]):
+            item["Index"] = 0
+            item["Enabled"] = true
+        if item["Name"] == pxe_device:
+            item["Index"] = 1
+            item["Enabled"] = true
+        if item["Index"] == 0 and not re.match("Disk", item["Name"]):
+            item["Index"] = expected_pxedev_current_index
+            item["Enabled"] = true
 
 with open("boot_devices.txt", "w") as f:
     json.dump(boot_array, f)
+
+# exit 2 means update the bios
+sys.exit(2)
  
